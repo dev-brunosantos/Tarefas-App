c1f8ca8e7545104149df2e5a7ef13a9126372952
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-19 11:35:30 -0300

    Criando a logica de troca de tela automatica

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,20 @@
 import { Text } from "react-native";
 import { Container, Tela } from "../components/AppComponents";
+import { useEffect } from "react";
+import { router } from "expo-router";
 
 export default function App() {
+
+    useEffect(() => {
+        function mudarTela() {
+            setTimeout(() => {
+                return router.push('./Login')
+            }, 1000)
+        }
+
+        mudarTela()
+    })
+
     return (
         <Tela>
             <Container>
