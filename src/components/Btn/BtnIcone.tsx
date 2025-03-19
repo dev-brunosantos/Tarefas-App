949d5f47c69b07cecc2d7157880210332dd487da
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-19 11:16:36 -0300

    Criando estilos do componente Botão Icone

--- src/components/Btn/BtnIcone.tsx
+++ src/components/Btn/BtnIcone.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { StyleSheet, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Entypo } from '@expo/vector-icons';
 
 interface BtnIconeProps extends TouchableOpacityProps {
@@ -9,8 +9,20 @@ export const BtnIcone = ({ icone, ...rest }:BtnIconeProps) => {
     return(
         <TouchableOpacity 
             {...rest}
+            style={styles.btnInput}
         >
             <Entypo name={icone} size={35} /> 
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    btnInput: {
+        height: '100%',
+        padding: 10,
+        position: 'absolute',
+        right: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
