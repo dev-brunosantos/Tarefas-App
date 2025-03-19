251ff00648ee1a921654da168f5fed5b242017b8
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-19 11:52:34 -0300

    Criando o arquivo responsável por armazenar as informações de contextos

--- src/contexts/AppContextsProvider.tsx
+++ src/contexts/AppContextsProvider.tsx
@@ -1,3 +1,10 @@
-export const AppContextProvider = () => {
-    return()
+import { ReactNode } from "react"
+import { LoginProvider } from "./LoginContext"
+
+export const AppContextProvider = ({ children }: { children: ReactNode }) => {
+    return (
+        <LoginProvider>
+            {children}
+        </LoginProvider>
+    )
 }
